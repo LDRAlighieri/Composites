78c5792bbf72d162c349c8e802fa5f3cdfa83932
Vladimir Raupov <GLDRAlighieri@gmail.com>
2023-06-23 19:46:55 +0300

    Remove DSL_SCOPE_VIOLATION suppress

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,6 @@
 
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
-// https://youtrack.jetbrains.com/issue/KTIJ-19369
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
