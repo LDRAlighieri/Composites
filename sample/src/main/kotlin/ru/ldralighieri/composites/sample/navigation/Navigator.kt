4dcefd7a1a2a8bd24398c30499f27a1c9b0fb5ca
Vladimir Raupov <GLDRAlighieri@gmail.com>
2023-12-26 18:29:01 +0300

    Fixed duplication of events in the sample app navigator

--- sample/src/main/kotlin/ru/ldralighieri/composites/sample/navigation/Navigator.kt
+++ sample/src/main/kotlin/ru/ldralighieri/composites/sample/navigation/Navigator.kt
@@ -18,21 +18,21 @@ package ru.ldralighieri.composites.sample.navigation
 
 import android.os.Parcelable
 import androidx.compose.runtime.staticCompositionLocalOf
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
 import ru.ldralighieri.composites.carbon.core.Destination
 
 class Navigator {
 
-    private val _destinations = MutableSharedFlow<Event>(replay = 1)
-    val destinations: SharedFlow<Event> = _destinations.asSharedFlow()
+    private val _destinations = Channel<Event>()
+    val destinations: Flow<Event> = _destinations.receiveAsFlow()
 
     fun navigateTo(destination: Destination) {
-        _destinations.tryEmit(Event.ToDestination(destination))
+        _destinations.trySend(Event.ToDestination(destination))
     }
 
-    fun navigateBack() { _destinations.tryEmit(Event.Back()) }
+    fun navigateBack() { _destinations.trySend(Event.Back()) }
 
     sealed interface Event {
         data class ToDestination(val destination: Destination) : Event
