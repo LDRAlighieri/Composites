27637a9631924e202ff6f03e246388e406f39953
Vladimir Raupov <GLDRAlighieri@gmail.com>
2025-06-27 22:13:05 +0300

    Update kotlin to v2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,12 +17,12 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.jetbrains.compose) apply false
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.spotless) apply false
     alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.ksp) apply false
